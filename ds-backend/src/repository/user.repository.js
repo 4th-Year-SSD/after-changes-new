6227c2121e5181f779b506bb0458d079c7ff213a
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-10-03 01:17:31 +0530

    Fix mongoose populate bug

--- ds-backend/src/repository/user.repository.js
+++ ds-backend/src/repository/user.repository.js
@@ -84,7 +84,9 @@ export const getAllUsers = async ({
 
 export const getOneUser = async (filters, returnPassword = false) => {
   const user = await User.findOne(filters)
-    .populate("buyer seller admin")
+    .populate("buyer")
+    .populate("seller")
+    .populate("admin")
     .lean();
   if (!user) return null;
 
